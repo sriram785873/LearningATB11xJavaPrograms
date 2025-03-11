2be93a72ca48e6ae36f04228e5a7655c03abc6a9
sriram785873 <ram785873@gmail.com>
2025-03-11 16:20:27 +0530

    March 11th code

---
+++ src/ex_01_Java_Basic/Lab001_Mar11.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basic;
+
+public class Lab001_Mar11 {
+    public static void main(String[] args) {
+
+        int a=9;
+        //System.out.printf("%d*1=",a,a*1);
+
+        System.out.println(a+"*1"+"="+ a*1);
+        System.out.println(a+"*2"+"="+ a*2);
+        System.out.println(a+"*3"+"="+ a*3);
+
+    }
+
+
+}
